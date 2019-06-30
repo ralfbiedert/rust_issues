49756f40855070a5526cbc9f8d812a47ebe36b37
Ralf Biedert <rb@xr.io>
2019-06-30 21:42:19 +0200

    Rayon ThreadPool w/o allocation WIP.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 
+# Basic Problem
+
 ```
 === Allocation Statistics ===
 "()" -- Events: 0, Bytes: 0
@@ -9,3 +11,44 @@
 "pool.install(|| {}) -- next 1000 times" -- Events: 2032, Bytes: 48064
 "drop(pool)" -- Events: 0, Bytes: 0
 ```
+
+
+# Source of Allocation
+
+- `ThreadPool::install` invokes `Registry::in_worker`
+- There the check `worker_thread.is_null()` constantly fails, so it invokes `self.in_worker_cold(op)`.
+- That in turn does a `LockLatch::new()`, which causes the following allocations: 
+
+```
+"Mutex::new" -- Events: 1, Bytes: 16
+"Condvar::new" -- Events: 1, Bytes: 8
+```
+
+- The `worker_thread.is_null` check fails because `WorkerThread::current()` always returns `0x0`.
+
+- Which is a bit puzzling, because I can observe that `WorkerThread::set_current(worker_thread);` is invoked from `main_loop` (multiple times),
+and 
+
+```
+println!("main_loop() - before set {:?}", WorkerThread::current());
+WorkerThread::set_current(worker_thread);
+println!("main_loop() - after set {:?}", WorkerThread::current());
+``` 
+
+- each prints something like: 
+
+```
+main_loop() - before set 0x0
+main_loop() - after set 0x635feff240
+```                                           
+
+
+
+
+# Trying to Fix
+
+
+- Putting LockLatch on TLS technically works, but `Condvar` doesn't reset so can't be reused.
+- Have to replace condvar.
+
+ 
--- src/main.rs
+++ src/main.rs
@@ -1,111 +1,29 @@
-#![feature(allocator_api)]
-
 use rayon::ThreadPoolBuilder;
-use std::alloc::{GlobalAlloc, Layout, System};
-use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
-use std::thread::ThreadId;
-
-static mut EVENTS: [Event; 1000000] = [Event::new(); 1000000];
-static mut NEXT: AtomicUsize = AtomicUsize::new(0);
-static mut TRACK: AtomicBool = AtomicBool::new(false);
-
-pub struct UserAlloc;
-
-#[global_allocator]
-static mut ALLOCATOR: UserAlloc = UserAlloc;
-
-#[derive(Debug, Copy, Clone)]
-struct Event {
-    thread_id: Option<ThreadId>,
-    time: usize,
-    size: usize,
-    align: usize,
-}
-
-impl Event {
-    const fn new() -> Self {
-        Self {
-            thread_id: None,
-            time: 0,
-            size: 0,
-            align: 0,
-        }
-    }
-}
-
-unsafe impl GlobalAlloc for UserAlloc {
-    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        if TRACK.load(Ordering::SeqCst) {
-            // let x = thread::current(); // crash ?!
-            EVENTS[NEXT.load(Ordering::SeqCst)] = Event {
-                thread_id: None,
-                size: layout.size(),
-                align: layout.align(),
-                time: 0,
-            };
-
-            NEXT.fetch_add(1, Ordering::SeqCst);
-        }
-
-        System.alloc(layout)
-    }
-
-    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        unsafe { System.dealloc(ptr, layout) }
-    }
-}
-
-fn measure<T: FnOnce() -> R, R>(label: &str, x: T) -> R {
-    unsafe {
-        TRACK.store(true, Ordering::SeqCst);
-
-        let r = x();
-
-        TRACK.store(false, Ordering::SeqCst);
-
-        let N = NEXT.load(Ordering::SeqCst);
-        let mut total_bytes = 0;
-
-        for i in 0..N {
-            let event = EVENTS[i];
-
-            total_bytes += event.size;
-        }
-
-        println!("{:?} -- Events: {:?}, Bytes: {:?}", label, N, total_bytes);
-
-        NEXT.store(0, Ordering::SeqCst);
-
-        r
-    }
-}
+use std::thread::sleep;
+use std::time::Duration;
 
 pub fn main() {
     // DON'T TRY THIS AT HOME, KIDS
     unsafe {
         println!("=== Allocation Statistics ===");
 
-        measure("()", || ());
+        let pool = ThreadPoolBuilder::new().num_threads(4).build().unwrap();
 
-        measure("Box::new(1)", || {
-            Box::new(1);
-        });
+        pool.install(|| {});
+        pool.install(|| {});
 
-        let mut pool = measure(
-            "pool = ThreadPoolBuilder::new().num_threads(4).build()",
-            || ThreadPoolBuilder::new().num_threads(4).build().unwrap(),
-        );
+        //        println!("Waiting 1 second");
+        sleep(Duration::new(1, 0));
 
-        measure("pool.install(|| {}) -- 1st time", || pool.install(|| {}));
+        for i in 0..100 {
+            pool.install(|| {})
+        }
 
-        measure("pool.install(|| {}) -- 2nd time", || pool.install(|| {}));
+        //        println!("Waiting 1 second");
+        sleep(Duration::new(1, 0));
 
-        measure("pool.install(|| {}) -- next 1000 times", || {
-            for i in 0..1000 {
-                pool.install(|| {})
-            }
-        });
-
-        measure("drop(pool)", || drop(pool));
+        for i in 0..100 {
+            pool.install(|| {})
+        }
     }
 }
