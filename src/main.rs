3ef2370a83308ea35933e294af7af90d056bfd82
Ralf Biedert <rb@xr.io>
2019-08-16 16:02:52 +0200

    Attempting to reproduce SIGILL.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
-use serde_derive::Serialize;
+use ffsvm::{DenseProblem, DenseSVM};
+use ndarray::arr1;
+use prophet::topology::{Topology, TopologyBuilder};
+use prophet::NeuralNet;
+use prophet::Predict;
+use std::convert::TryFrom;
 
-#[derive(Serialize)]
-enum E {
-    // `crate` seems to be causing this
-    X,
+fn main() {
+    let model = DenseSVM::try_from(include_str!("../dummy.model")).unwrap();
+    let mut problem = DenseProblem::from(&model);
+    let features = problem.features();
+    let slice = features.as_slice_mut();
+
+    let model = include_bytes!("../net");
+    let mut net: NeuralNet = serde_json::from_slice(model).unwrap();
+
+    let x = arr1(features.as_slice_mut());
+
+    // Same code causes SIGILL in other project ... why doesn't it here?
+    let base = net.predict(&x);
 }
-
-#[derive(Debug, Clone, Copy, Serialize)]
-#[repr(C)]
-pub struct Matrix4f32 {
-    crate data: [f32; 16],
-}
-
-fn main() {}
