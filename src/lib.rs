df016b8767c0cf2e35ba4976cd79a2d121829876
Ralf Biedert <rb@xr.io>
2020-02-01 12:27:05 +0100

    Alright, syntax errors...

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,34 @@ struct S {
 
 fn g() -> Result<S, ()> {
     let db = new_db();
-    
- // Ok(S { db: db? })
-    Ok(S { db? })
+
+    if db.is_err() {
+        // We need special handling here so writing
+        // `new_db()?` wasn't an option.
+    }
+
+    Ok(S { db: db? })
+    //    Ok(S { db? })
 }
 
 fn new_db() -> Result<u8, ()> {
     Ok(0)
 }
+
+/*
+error: expected one of `,` or `}`, found `?`
+--> src\lib.rs:14:14
+|
+14 |     Ok(S { db? })
+|        -     ^ expected one of `,` or `}`
+|        |
+|        while parsing this struct
+
+error[E0063]: missing field `db` in initializer of `S`
+--> src\lib.rs:14:8
+|
+14 |     Ok(S { db? })
+|        ^ missing `db`
+
+error: aborting due to 2 previous errors
+*/
