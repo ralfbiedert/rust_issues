57ee9ca3e68514a07d09af7b7eb2de3de62cceab
Ralf Biedert <rb@xr.io>
2020-03-10 23:03:05 +0100

    Minimal example to provoke https://github.com/rust-lang/rust/issues/53275

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,8 @@
-struct S {
-    db: u8,
+#[derive(Debug, PartialEq)]
+struct S(u8);
+
+#[test]
+fn ice() {
+    let s = S(0);
+    assert_eq!(s, S([][0]));
 }
-
-fn g() -> Result<S, ()> {
-    let db = new_db();
-
-    if db.is_err() {
-        // We need special handling here so writing
-        // `new_db()?` wasn't an option.
-    }
-
-    Ok(S { db: db? })
-    //    Ok(S { db? })
-}
-
-fn new_db() -> Result<u8, ()> {
-    Ok(0)
-}
-
-/*
-error: expected one of `,` or `}`, found `?`
---> src\lib.rs:14:14
-|
-14 |     Ok(S { db? })
-|        -     ^ expected one of `,` or `}`
-|        |
-|        while parsing this struct
-
-error[E0063]: missing field `db` in initializer of `S`
---> src\lib.rs:14:8
-|
-14 |     Ok(S { db? })
-|        ^ missing `db`
-
-error: aborting due to 2 previous errors
-*/
